c80fa237358eed3bd2e5919640a82b3261c19c1e
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 14:34:13 +0100

    Update InputWindow.java

--- InputWindow.java
+++ InputWindow.java
@@ -1,5 +1,17 @@
 package codebreaker;
 
+/*
+* I have created input and output Windows applications which are a custom made java windows designed to take input from user or display some information as output to the user. 
+* They have been designed and written by using Java Swing Components like Jarea, Jlabel, Jbutton, Jframe etc. 
+* I have also used notifyAll() method to make an application wait for users input.
+*
+* - You may use this Class in your application for non commercial use if you wish but you have to keep this comment below :
+*
+* @author Michal Switala
+* Copyright © 2021 belongs solely to Michal Switala. 
+* You can reach me out on : https://github.com/michaelPro89
+*/
+
 /*****************************************************************
  * Author: Michal Switala 
  * Version: 4 
@@ -39,15 +51,6 @@ import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import javax.swing.ImageIcon;
 
-/*
-* I have created input and output Windows applications which are a custom made java windows designed to take input from user or display some information as output to the user. 
-* They can be used in any project by anyone. They have been designed and written by using Java Swing Components like Jarea, Jlabel, Jbutton, Jframe etc. 
-* I have also used notifyAll() method to make an application wait for users input.
-*
-* @author Michal Switala
-* * Copyright © 2021 belongs solely to Michal Switala. 
-* You can reach me out on : https://github.com/michaelPro89
-*/
 
 class InputWindow implements ActionListener {
 
